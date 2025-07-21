84ec6aaa91fe3a8a0c398377c926e32b52a8bcdb
Student-Amansahu <159453724+Student-Amansahu@users.noreply.github.com>
2025-07-21 17:16:35 +0530

    in change

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -5,8 +5,8 @@ function Card() {
   return (
     <>
       <div className='  bg-[rgb(251,251,251)] rounded-2xl mt-2 h-100 flex'>
-        <div className='w-1/2 h-full bg-violet-400 flex flex-col justify-center pl-10 '>
-          <p className='text-[13px]'>Order Restaurant food, takeaway and groceries.</p>
+        <div className='w-1/2 h-full bg-violet-400 flex flex-col justify-center pl-5 '>
+          <p className='text-[15px]  text-white'>Order Restaurant food, takeaway and groceries.</p>
           <p>Feast Your Senses, <span>Fast and Fresh</span></p>
           <form>
             <label htmlFor="text">Enter a passcode to see what be deliver</label>
