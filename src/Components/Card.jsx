d3e7fdfae3f29b0ff5e1823fc185b67222df9c70
Student-Amansahu <159453724+Student-Amansahu@users.noreply.github.com>
2025-07-21 17:04:14 +0530

    in change

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,22 +4,21 @@ import React from 'react'
 function Card() {
   return (
     <>
-    <div className='  bg-[rgb(251,251,251)] rounded-2xl mt-2 h-100 flex'>
-      <div className='w-1/2 h-full bg-violet-400 '>
-      <p>Order Restaurant food, takeaway and groceries.</p>
-      <p>Feast Your Senses, <span>Fast and Fresh</span></p>
-      <form>
-       <label htmlFor="text">Enter a passcode to see what be deliver</label>
-       <div>
-       <input type="text" />
-       <button>Search</button>
-       </div>
-      </form>
-     
-      </div>
-      <div className=' w-1/2 bg-gray-600'>h</div>
+      <div className='  bg-[rgb(251,251,251)] rounded-2xl mt-2 h-100 flex'>
+        <div className='w-1/2 h-full bg-violet-400 flex flex-col justify-center pl-10 '>
+          <p className='text-[13px]'>Order Restaurant food, takeaway and groceries.</p>
+          <p>Feast Your Senses, <span>Fast and Fresh</span></p>
+          <form>
+            <label htmlFor="text">Enter a passcode to see what be deliver</label>
+            <div>
+              <input type="text" />
+              <button>Search</button>
+            </div>
+          </form>
+        </div>
+        <div className=' w-1/2 bg-gray-600'>h</div>
 
-    </div>
+      </div>
     </>
   )
 }
