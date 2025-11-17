dce2c78ac9868cda4c9bb2d456c4043090412033
Student-Amansahu <159453724+aman-sahu-31@users.noreply.github.com>
2025-11-17 11:43:18 +0530

    yes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [ tailwindcss(),react()],
@@ -9,4 +10,4 @@ export default defineConfig({
   build: {
     outDir: 'dist'
   }
-})
+});
